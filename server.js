bbe4a038385c38f54520dd41cbd7de08150921ae
indu-54 <150770818+indu-54@users.noreply.github.com>
2024-04-02 15:24:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const cors = require('cors')
 const users = require('./routes/users');
 const { generateOTP } = require('./utils/otp');
