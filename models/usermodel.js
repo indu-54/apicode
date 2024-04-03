0d00dc4973c3382ada853f717bfaf1a1989cc196
indu-54 <150770818+indu-54@users.noreply.github.com>
2024-04-03 10:44:11 +0530

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,9 +1,9 @@
 
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const usermodelSchema = new mongoose.Schema({
-    
+
+
     firstname: {type: String, required: true, },
     lastname: {type: String, required: true,},
     email: {type: String,required: true,unique: true,},
