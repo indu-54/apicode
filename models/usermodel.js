be77a07e5f70a88256603652e15edc27dcc7ab5e
indu-54 <150770818+indu-54@users.noreply.github.com>
2024-04-03 13:41:38 +0530

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,18 +1,14 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const usermodelSchema = new mongoose.Schema({
 
-
-    firstname: {type: String, required: true, },
-    lastname: {type: String, required: true,},
-    email: {type: String,required: true,unique: true,},
-    passwordHash: { type: String, required: true,},
-    confirmpasswordHash: {  type: String, required: true, },
-    mobileNumber: { type: Number, unique: true,},
-    address: {type: String,required: true,},
-    otp: { type: Number, unique:true},
-   
+const usermodelSchema = new Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    mobileNumber: { type: Number, unique: true },
+    address: { type: String, required: true },
+    otp: { type: Number, required: true, unique: true }
 });
 
 module.exports = mongoose.model('UserModel', usermodelSchema);
