2920657cec62a3b7eecbb0346e1cc441315f9801
indu-54 <150770818+indu-54@users.noreply.github.com>
2024-04-03 10:45:13 +0530

    Update otp.js

--- utils/otp.js
+++ utils/otp.js
@@ -1,17 +1,17 @@
-// utils/otp.js
+// // utils/otp.js
 
-// Function to generate a random OTP
-function generateOTP() {
-    const length = 6; // Length of the OTP
-    const charset = '0123456789'; // Character set for the OTP
+// // Function to generate a random OTP
+// function generateOTP() {
+//     const length = 6; // Length of the OTP
+//     const charset = '0123456789'; // Character set for the OTP
 
-    let otp = '';
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        otp += charset[randomIndex];
-    }
+//     let otp = '';
+//     for (let i = 0; i < length; i++) {
+//         const randomIndex = Math.floor(Math.random() * charset.length);
+//         otp += charset[randomIndex];
+//     }
 
-    return otp;
-}
+//     return otp;
+// }
 
-module.exports = { generateOTP };
+// module.exports = { generateOTP };
